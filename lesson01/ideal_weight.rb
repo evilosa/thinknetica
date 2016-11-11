af55aa236e77b8fecf4752322a0e1248f64ba544
Bozhkov Aleksandr <bojkov@mbconsalting.ru>
2016-11-11 08:20:06 +0300

    Update ideal_weight.rb

--- lesson01/ideal_weight.rb
+++ lesson01/ideal_weight.rb
@@ -6,5 +6,5 @@ print 'Input your height: '
 height = gets.chomp.to_f
 ideal_weight = height - 110
 
-puts "#{name} your weight is #{height}" if ideal_weight >= 0
+puts "#{name} your weight is #{ideal_weight}" if ideal_weight >= 0
 puts "#{name} your weight is ideal" if ideal_weight < 0
